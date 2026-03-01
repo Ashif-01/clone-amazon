8da9eeb0db5a708e0f4206fd750ad53d8cca7e3c
Ashif-01 <ashifhussainansari75@gmail.com>
2026-03-01 05:30:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 // Track the number of items in the cart
 let cartCount = 0;
 
-// Function triggered when 'Add to Cart' is clicked
+// Function triggered when the Cart area is clicked
 function addToCart() {
-    cartCount++; // Increase the count by 1
-    // Update the HTML text inside the cart-count span
+    cartCount++;
     document.getElementById('cart-count').innerText = cartCount;
+    
+    // Optional: Add a little animation or alert for feedback
+    console.log("Item added to cart. Total items: " + cartCount);
+}
+
+// Function for the search button
+function triggerSearch() {
+    const searchInput = document.querySelector('.search-input').value;
+    if (searchInput) {
+        alert("Searching Amazon for: " + searchInput);
+    } else {
+        alert("Please enter a product to search.");
+    }
 }
